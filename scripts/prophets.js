cbc494740ab5bf68df7c26b2f315274b5918993b
yourusernameCave <dracothedragon9292@gmail.com>
2025-09-10 17:04:44 -0600

    w2u02

---
+++ scripts/prophets.js
@@ -0,0 +1,8 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
